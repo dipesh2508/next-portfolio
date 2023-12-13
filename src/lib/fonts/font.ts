10374dbc26e5efd2c4a18a98d1b206ed0df30553
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2023-12-13 18:34:59 +0530

    font and color setup

---
+++ src/lib/fonts/font.ts
@@ -0,0 +1,6 @@
+import { Whisper } from "next/font/google";
+
+export const whisper = Whisper({
+  weight: "400",
+  subsets: ["latin"],
+});
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -30,6 +30,23 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      color: {
+          'primary': '#17B890',
+          'secondary': '#66D7D1',
+          'cta': '#8B5FBF',
+          'bg-1': '#181616',
+          'bg-2': '#1E1E1E',
+          'light-1': '#F2F2F2',
+          glassmorphism: "rgba(16, 16, 18, 0.60)",
+      },
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"],
+        serif: ['var(--font-caudex)', 'serif'],
+      },
+      boxShadow: {
+        "count-badge": "0px 0px 6px 2px rgba(219, 188, 159, 0.30)",
+        "groups-sidebar": "-30px 0px 60px 0px rgba(28, 28, 31, 0.50)",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
