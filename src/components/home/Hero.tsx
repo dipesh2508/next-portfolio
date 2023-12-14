e1d3d88ffef50683860a10f5bf8998ac3724902d
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2023-12-14 15:05:59 +0530

    Hero section responsive

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,35 +1,46 @@
 import { Button } from "../ui/button";
 import { whisper } from "@/lib/fonts/font";
 import Image from "next/image";
-import hero from '@/assets/Hero.png'
+import hero from "@/assets/Hero.png";
 
-import ParticleBackground from "../design/ParticleBackground";
+import ParticleBackground from "@/components/design/ParticleBackground";
 
 const Hero = () => {
-
   return (
     <>
-    
-    <section id="hero" className="hero grid h-screen grid-cols-2 text-light-1  text-right items-center pt-20">
-      <div className="flex flex-col ml-20">
-        <div className="text-base italic">I am <span className="text-secondary">Dipesh Ranjan</span></div>
-        <div className="text-8xl font-serif pb-8">
-          Web <span className="text-primary">Dev</span>eloper <br /> <span className={`text-8xl ${whisper.className}`}>& <span className="text-cta">Design</span>er</span>
+      <section
+        id="hero"
+        className="grid h-screen w-full grid-cols-1 items-center pt-20 text-center text-light-1 md:grid-cols-2 md:text-right"
+      >
+        <div className="mx-10 flex flex-col md:mx-0 md:ml-20">
+          <div className="text-xs italic md:text-base">
+            I am <span className="text-secondary">Dipesh Ranjan</span>
+          </div>
+          <div className="pb-4 font-serif text-4xl md:pb-8 md:text-8xl">
+            Web <span className="text-primary">Dev</span>eloper <br />{" "}
+            <span className={`text-4xl md:text-8xl ${whisper.className}`}>
+              & <span className="text-cta">Design</span>er
+            </span>
+          </div>
+          <div className="md:font-base text-xs italic  md:text-base">
+            I weave together threads of creativity and logic, crafting expansive
+            websites that sing with beauty, respond to every need, and delight
+            the user&#39;s senses.
+          </div>
+          <div className="mt-8 flex flex-row justify-center gap-3 md:justify-end md:gap-8">
+            {" "}
+            <Button>Download CV</Button>
+            <Button variant="cta">Let&#39;s Talk</Button>{" "}
+          </div>
         </div>
-        <div className="italic font-base">
-          I weave together threads of creativity and logic, crafting expansive
-          websites that sing with beauty, respond to every need, and delight the
-          user&#39;s senses.
+        <div className="flex mx-8 items-center h-72 w-72 -mt-16 md:mt-0 md:mx-0 md:ml-16 md:h-[500px] md:w-[500px]">
+          {" "}
+          <Image src={hero} alt="dipesh" />
         </div>
-        <div className="flex flex-row gap-8 justify-end mt-8"> <Button>Download CV</Button>
-        <Button variant="cta">Let&#39;s Talk</Button> </div>
-        
+      </section>
+      <div className="relative -z-10">
+        <ParticleBackground />
       </div>
-      <div className="ml-16 h-[500px] w-[500px]"> <Image src={hero} alt="dipesh" /></div>
-    </section>
-    <div className="relative -z-10">
-    <ParticleBackground />
-    </div>
     </>
   );
 };
